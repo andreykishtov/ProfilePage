540bb95110d0523d93a35bc296627c5917105dd2
Andrey Kishtov <andreykishtov@gmail.com>
2018-03-29 22:18:27 +0300

    cranks

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import {
   Section,
   Wrapper,
@@ -7,17 +9,39 @@ import {
   StyledInfoTable
 } from '../../StyledFiles/About.styled';
 
+import crank from '../../images/crank.png';
+
 import InfoTable from './InfoTable';
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  animation: rotation 6s infinite linear;
+
+  @keyframes rotation {
+    from {
+      transform: rotate(${props => props.from + 'deg'});
+    }
+    to {
+      transform: rotate(${props => props.to +'deg'});
+    }
+  }
+`;
 
 const About = ({ data }) => {
   return (
     <Section>
       <Wrapper>
         <StyledMyInfo myInfo={data.myInfo} />
-        <StyledCoolInfo />
-        <StyledInfoTable>
-          <InfoTable />
-        </StyledInfoTable>
+        <div>
+          <Image from={'0'} to={'360'} src={crank} />
+          <Image from={'0'} to={'100'} src={crank} />
+          <Image from={'360'} to={'0'} src={crank} />
+          <Image from={'0'} to={'360'} src={crank} />
+          <Image from={'360'} to={'0'} src={crank} />
+        </div>
+        {/* <StyledCoolInfo /> */}
+        <StyledInfoTable>{/* <InfoTable /> */}</StyledInfoTable>
       </Wrapper>
     </Section>
   );
